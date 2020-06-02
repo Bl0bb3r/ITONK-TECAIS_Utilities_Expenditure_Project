218fcf897c749e9f123e96938e6c18d5ac2fb988
Mikkel Bleeg Carstensen <mikkelbleeg@hotmail.com>
2020-06-02 12:56:30 +0200

    Added HeatSubmission

---
+++ HeatConsumptionSubmission/Services/IPricingService.cs
@@ -0,0 +1,13 @@
+﻿using ElectricityConsumptionSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeatConsumptionSubmission.Services
+{
+    interface IPricingService
+    {
+        Task<PricingInformation> GetPricingInformationAsync();
+    }
+}
