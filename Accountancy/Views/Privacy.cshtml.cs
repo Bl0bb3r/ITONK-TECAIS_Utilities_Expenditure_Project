928106034f695c9bcc7152f87c849068c169f001
Mikkel Bleeg Carstensen <mikkelbleeg@hotmail.com>
2020-06-05 15:18:06 +0200

    .

--- Accountancy/Views/Privacy.cshtml.cs
+++ Accountancy/Views/Privacy.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AccountingControl.Pages
+namespace Accountancy.Views
 {
     public class PrivacyModel : PageModel
     {
